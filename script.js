b4cb5e9a1089a75c18bcfa93344e07a8c16148e6
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-08-01 14:15:11 +0200

    brought focus

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
-    if (principal < 1) {
-        alert('Enter a positive number')
-    }
+
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 
     document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    if (principal < 1) {
+        alert('Enter a positive number')
+        document.getElementById("principal").focus();
+        document.getElementById("result").innerHTML=""
+    }
 }
 
 function updateRate() 
